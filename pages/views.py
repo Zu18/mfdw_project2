d4e1a6a61fbc969d6995a46228019a809b138b3d
Zukhra <zuhra.galieva.2014@gmail.com>
2023-03-14 14:24:24 +0000

    add the changes in view.py

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 
 
 def index(request):
-    # return HttpResponse("<h1> The Meanco Homepage</h1>")
-    return render(request, 'base.html')
+    return render(request, 'pages/page.html')
 
 
 
