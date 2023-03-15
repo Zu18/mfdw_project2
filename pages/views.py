7f3c921fcdc2140f21974bf099b20096f17158e6
Zukhra <zuhra.galieva.2014@gmail.com>
2023-03-15 15:08:55 +0000

    Change view, urls and page.html

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from . models import Page
 
 
-def index(request):
-    return render(request, 'pages/page.html')
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_update': pg.update_date
+    }
+    # assert False
+    return render(request, 'pages/page.html', context)
 
 
 
